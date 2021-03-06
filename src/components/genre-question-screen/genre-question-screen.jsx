1f3e8b4d4af0cff0aac5b5d401c80b36f031b491
Yuriy Marin <yuriy.marin@gmail.com>
2021-03-06 16:53:31 +0300

    feat: Компонент mistakes добавлен в игровые экраны

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -5,7 +5,7 @@ import genreQuestionProp from './genre-question.prop';
 
 const GenreQuestionScreen = (props) => {
   const [userAnswers, setUserAnswers] = useState([false, false, false, false]);
-  const {onAnswer, question, renderPlayer} = props;
+  const {onAnswer, question, renderPlayer, children} = props;
   const {answers, genre} = question;
 
   return (
@@ -21,11 +21,7 @@ const GenreQuestionScreen = (props) => {
             style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
         </svg>
 
-        <div className="game__mistakes">
-          <div className="wrong"/>
-          <div className="wrong"/>
-          <div className="wrong"/>
-        </div>
+        {children}
       </header>
 
       <section className="game__screen">
@@ -66,7 +62,8 @@ const GenreQuestionScreen = (props) => {
 GenreQuestionScreen.protoTypes = {
   onAnswer: PropTypes.func.isRequired,
   question: genreQuestionProp,
-  renderPlayer: PropTypes.func.isRequired
+  renderPlayer: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired
 };
 
 export default GenreQuestionScreen;
