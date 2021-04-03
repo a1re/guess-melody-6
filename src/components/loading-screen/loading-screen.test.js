7a5c2bdc4f61be43bb93964f595625e61f5b1cea
Yuriy Marin <yuriy.marin@gmail.com>
2021-04-03 14:35:24 +0300

    feat: Добавлены снепшот-тесты простых компонентов

---
+++ src/components/loading-screen/loading-screen.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import LoadingScreen from './loading-screen';
+
+it(`Should Loading Screen render correctly`, () => {
+  const {container} = render(<LoadingScreen />);
+  expect(container).toMatchSnapshot();
+});
